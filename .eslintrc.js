eef8abf51f4552f34f01776db6a8b26cb9a6a53f
Misha <mishapro@mm.st>
2020-03-26 19:50:13 +0100

    Build : Add prettier and fix eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,10 @@ module.exports = {
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
     plugins: ['@typescript-eslint'],
     extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        'plugin:prettier/recommended', // Stay at the last
     ],
     parserOptions: {
         ecmaVersion: 2019, // Allows for the parsing of modern ECMAScript features

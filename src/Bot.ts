535cfc642baf94af681e47815a87b9122da65afd
Misha <mishapro@mm.st>
2020-03-27 08:54:23 +0100

    Feat : Make Event manager and set up Discord clien
    The event manager class is the class which allows to manage all the events of the bot. It will eventually allow to load event files and to define them.

---
+++ src/Bot.ts
@@ -0,0 +1,25 @@
+import * as Discord from 'discord.js';
+import { EventManager } from './Core/EventManager';
+import { Events } from './Events';
+
+export class Bot {
+    private static instance: Bot;
+    private client: Discord.Client;
+
+    private constructor(client: Discord.Client) {
+        this.client = client;
+        const manager = EventManager.getInstance(this.client);
+        const event = new Events();
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        manager.defineOn('ready', event.onReady).defineOn('message', event.onMessage);
+
+        this.client.login(process.env.TOKEN);
+    }
+
+    public static getInstance(): Bot {
+        if (!this.instance) {
+            this.instance = new Bot(new Discord.Client());
+        }
+        return this.instance;
+    }
+}
